b7a31755b3fe0f10486e760e4cb2aa54a78f3592
Gauravsingh0712 <88282785+Gauravsingh0712@users.noreply.github.com>
2021-09-01 21:41:00 +0530

    Add files via upload

---
+++ DSA series/linkedList/intro.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+public:
+	int data;
+	node* next;
+	node(int val)
+	{
+		data = val;
+		next = NULL;
+	}
+};
+
+void insertAtTail(node* &head, int val)
+{
+	node* n = new node(val);
+	if (head == NULL)
+	{
+		head = n;
+		return;
+	}
+	node* temp = head;
+	while(temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = n;
+}
+
+void display(node* head)
+{
+	node* temp = head;
+	while(temp != NULL)
+	{
+		cout<< temp->data <<"->";
+		temp = temp->next; 
+	}
+	cout<< "NULL" <<endl;
+}
+
+void insertAtHead(node* &head, int key)
+{
+	node* n = new node(key);
+	n->next = head;
+	head = n;
+}
+
+void deletion(node* &head , int val)
+{
+	if(head == NULL)
+	{
+		return;
+	}
+	node* temp = head ;
+	while(temp->next->data != val)
+	{
+		temp = temp->next;
+	}
+	node* todelete = temp->next;
+	temp->next = temp->next->next;
+	delete todelete;
+}
+node* reverse(node* &head)
+{
+	node* prevptr = NULL;
+	node* currptr = head;
+	node* nextptr;
+	while(currptr != NULL)
+	{
+		nextptr = currptr->next;
+		currptr->next = prevptr;
+
+		prevptr = currptr;
+		currptr = nextptr;
+	}
+	return prevptr;
+}
+
+int main()
+{
+	node* head = NULL;
+	insertAtTail(head, 1);
+	insertAtTail(head, 2);
+	insertAtTail(head ,3);
+	insertAtTail(head,4);
+	display(head);
+	node* newhead = reverse(head);
+	display(newhead);
+
+	return 0;
+}
